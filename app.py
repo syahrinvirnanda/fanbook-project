c3da443e565e9ec004d9b121827a36ce19ecb983
syahrin virnanda <97946529+syahrinvirnanda@users.noreply.github.com>
2022-12-04 22:43:16 +0700

    flask setup for depoloyment

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://syahrinvirnanda:syahrin123@cluster0.ar4suzr.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI= os.environ.get("MONGODB_URI")
+DB_NAME= os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,6 +35,6 @@ def homework_post():
 def homework_get():
     message_list = list(db.fanmessages.find({}, {'_id': False}))
     return jsonify({'messages': message_list})
-    
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
